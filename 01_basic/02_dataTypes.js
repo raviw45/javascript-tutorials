c43eb0b090d24f54229c690860157dec2cff5e4e
Ravi Waghmare <122720689+raviw45@users.noreply.github.com>
2023-09-18 08:52:55 +0000

    conversion

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,34 @@
+//alert(3+3); written in nodejs not in browser
+let na="Ravikant";
+let am=23;
+am="amit";//we can assign the string to the already assigned number variable datatype.
+
+console.log(na);//we can assign string, number, boolean type to the var, let and const keyword
+console.log(am);
+
+//var --function scoped variable
+//let -- it is a block scoped variable
+
+
+//const --const variable
+
+let vas=true;
+console.log(vas);
+
+
+//symbol => unique
+
+let ss=Symbol("amar");
+console.log(typeof ss);
+
+
+//it is an object type but unique
+ ss=Symbol("amit");
+console.log(ss);
+
+
+
+
+
+
+
---
+++ 01_basic/03_conversionType.js
@@ -0,0 +1,24 @@
+// converting the string to the number
+let stringName="123";
+
+console.log(typeof stringName);
+
+let NumberString=Number(stringName);
+console.log(NumberString+23);
+
+
+//converting to the number to string
+let NumberToString=111222;
+const StringNumber=String(NumberToString);
+console.log(StringNumber+"amit");
+
+
+//convert boolean to number
+let NumberToBoolean=false;
+const booleanValue=Number(NumberToBoolean);
+console.log(booleanValue);//if true it will print 1 and if wrong then it will print 0
+
+//convert boolean to string 
+let BooleanTOString=true;
+const StringFromBoolean=String(BooleanTOString);
+console.log(StringFromBoolean+" Seconds");
