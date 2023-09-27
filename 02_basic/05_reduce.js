5dea437c9766c0f7a757f767084387a04e6f1a09
Ravi Waghmare <122720689+raviw45@users.noreply.github.com>
2023-09-27 07:05:56 +0000

    reducer_practice

--- 02_basic/05_reduce.js
+++ 02_basic/05_reduce.js
@@ -12,3 +12,24 @@ console.log(sumArr);
 
 let totalSumArr=arr.reduce((acc,currValue)=>acc+currValue,0);
 console.log(totalSumArr);
+
+/*what is diffenrence between map, filter and reduce?
+
+  map returns the array by modifying the exiting item.
+  filter returns the exiting array by true false condition.
+  reduce returns the reduced or compressed value in the single unit.
+*/
+
+const pracArrr=[1,2,3,4,5,6];
+const result=pracArrr.map(item=>item*item).map(item=>item+1);
+console.log(result);
+
+
+
+const result2=pracArrr.filter(item=>item>4);//it wont change the array value it will return the array on the basis of true and false condition
+console.log(result2);
+
+const result3=pracArrr.reduce((acc,item)=>acc+item,0);
+console.log(result3);//it will reduce the array and return the single unit value
+
+
