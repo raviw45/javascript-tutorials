de9f246a6de4be4814ded8618260477a7c98e81c
Ravi Waghmare <122720689+raviw45@users.noreply.github.com>
2023-10-06 12:07:04 +0000

    some renamed files

---
+++ 08_classes_and_oops/proto2.js
@@ -0,0 +1,53 @@
+// const myHeros={
+//     hulk:"hammer",
+//     spiderman:"sling",
+//     getPowerOfSpiderman:function(){
+//         console.log(`Spider: ${this.spiderman}`);
+//     }
+// }
+
+// Object.prototype.ravi=function(){
+//     console.log(`hello from ravi`);
+// }
+// myHeros.ravi()
+
+// const myFav=["ironman","thor"];
+
+// Array.prototype.ravikant=function(){
+//     console.log(`hello from the ravikant`);
+// }
+
+// // myFav.ravikant()
+// // myHeros.ravikant()  we can not access the prototype of the array but we can access the proto of object
+// //object above the heirarchy 
+// //object is the parent of function, array and string etc 
+
+
+// const User={
+//     name:"ravikant",
+//     email:"ravi@gmail.com"
+// }
+
+// const teacher={
+//     makeVideo:true
+// }
+
+// const teachingSupport={
+//     isAvailable:false
+// }
+
+// // const TASupport={
+// //     fullfill:'js support',
+// //     //__proto__:teachingSupport //using this syntax also we can achieve the inheritance
+// // }
+
+// // //using this method we can achieve the inheritance
+// //  Object.setPrototypeOf(TASupport,teachingSupport);
+// console.log(TASupport.isAvailable);
+// Object.setPrototypeOf(teacher,User);
+// console.log(teacher.name);
+// console.log(teacher.email);
+// console.log(teacher.makeVideo);
+
+
+
